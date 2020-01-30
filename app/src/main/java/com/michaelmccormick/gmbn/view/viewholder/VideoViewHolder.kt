3adc0dd22d14a99f71c914fb8718c215648eec1a
Michael McCormick <MichaelM97@live.co.uk>
2020-01-30 23:03:58 +0000

    Added navigation to detail fragment
    * Added SafeArgs dependencies

--- app/src/main/java/com/michaelmccormick/gmbn/view/viewholder/VideoViewHolder.kt
+++ app/src/main/java/com/michaelmccormick/gmbn/view/viewholder/VideoViewHolder.kt
@@ -1,8 +1,10 @@
 package com.michaelmccormick.gmbn.view.viewholder
 
 import android.view.View
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.michaelmccormick.gmbn.model.Video
+import com.michaelmccormick.gmbn.view.fragment.VideoListFragmentDirections
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.video_list_item.view.*
 
@@ -12,7 +14,14 @@ import kotlinx.android.synthetic.main.video_list_item.view.*
 class VideoViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
 
     fun bindVideo(video: Video) {
+        view.setOnClickListener { navigateToVideoDetails(it, video) }
         view.videoTitle.text = video.title
         Picasso.get().load(video.thumbnailUrl).into(view.thumbnail)
+    }
+
+    private fun navigateToVideoDetails(view: View, video: Video) {
+        val direction =
+            VideoListFragmentDirections.actionVideoListFragmentToVideoDetailFragment(video.id)
+        view.findNavController().navigate(direction)
     }
 }
